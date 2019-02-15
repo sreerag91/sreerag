705135fd3810ad34095801bbca0ab96896625d3e
sreerag91 <sreerag.temp@gmail.com>
2019-02-15 21:19:18 +0530

    modified

---
+++ break1.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+for (( a = 1 ; a < 4 ; a++ ))
+do
+	echo "Outer loop:$a"
+	for (( b = 1 ; b < 100 ; b++ ))
+	do
+		if [ $b -gt 4 ]
+		then 
+			break 1
+
+		fi
+		echo "Inner loop:$b"
+	done
+done
+
+
