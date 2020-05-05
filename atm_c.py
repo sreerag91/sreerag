6590ef4d48bfb195d77871d15e50d4c21e72793d
sreerag91 <sreerag.temp@gmail.com>
2020-05-05 13:07:07 +0530

    atm new update

--- atm_c.py
+++ atm_c.py
@@ -8,14 +8,18 @@ class Atm:
     
     self.pins =[1111,2222]
     self.pin = int(input("Please enter the pin number\n").strip())
+    self.trick= {"Name":"Sreerag","age":28,"place":"Kerala"}
+    self.trick1= {"Name":"Ramya","Age":25,"Place":"Karnataka"}
     if (self.pin == self.pins[0] or self.pin == self.pins[1]):
          self.choice=int(input("Enter 1 for withdraw or 2 for check balance:\n"))
          if (self.choice == 1):
              print("Your process is starting now")
              sreerag.withdraw()
+             sreerag.details()
          elif(self.choice == 2):
              print("wait")
              sreerag.balance()
+             sreerag.details()
          else:
              print("updating")
       
@@ -31,8 +35,8 @@ class Atm:
        elif (self.cash == 500):
           print("amount succefully debited is {}".format(self.cash))
        else:
-          print("Sorry not available available")
-
+          print("Sorry not available")
+          
 
  def balance(self):
    
@@ -40,5 +44,17 @@ class Atm:
        print("The balance is {}".format(self.cash))
 
 
+ def details(self):
+
+        print ("Details of customer")
+        if (self.pin == self.pins[0]):
+           for key,value in self.trick.items():
+               print(key,":",value)
+        elif (self.pin == self.pins[1]):
+            for key,value in self.trick1.items():
+               print(key,":",value)
+        else:
+               print("not available")
+ 
 sreerag = Atm()
 sreerag.start()
---
+++ pound.py
@@ -0,0 +1,20 @@
+class Pound:
+    def __init__(self,rare=False):
+        self.rare=rare
+
+        if self.rare:
+            self.value=1.25
+        else:
+            self.value=1.00
+
+        self.colour= "gold"
+        self.num_edges= 1
+        self.diameter= 22.5
+        self.thickness= 3.15
+        self.heads= True
+
+    def rust(self)
+        self.colour= "greenish"
+
+
+
